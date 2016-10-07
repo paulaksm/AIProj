3ec40d9325ff485f919ff8bd65db3e66b1f1c865
Qonrad <konradma@kth.se>
2016-10-07 11:04:36 +0200

    Added collision check in tree building phase.

--- environment.py
+++ environment.py
@@ -1,3 +1,4 @@
+#! /usr/bin/env python2
 '''
 Simple concept environment implemented in pygame.
 Testing if it would be suitable as some kind of
@@ -18,6 +19,7 @@ N_ROBOTS = 1
 ROBOT_WIDTH = 20
 ROBOT_HEIGHT = 20
 MAX_NODES = 10000
+STEP_LENGTH = 30.0
 
 screen = pygame.display.set_mode(SIZE);
 clock = pygame.time.Clock()
@@ -38,8 +40,10 @@ def init_map():
     "inits the map"
     global walls
 
-    walls.append(pygame.Rect((50,20),(120,240)))
+    walls.append(pygame.Rect((50,80),(120,320)))
+    walls.append(pygame.Rect((0,380),(50,20)))
     walls.append(pygame.Rect((400,200),(400,200)))
+    walls.append(pygame.Rect((200,0),(100,175)))
     
     for i in walls:
         pygame.draw.rect(screen, (100,100,100), i)
@@ -67,15 +71,15 @@ def goal_coll(p1, p2, radius):
 
 def new_step(p1, p2):
     "calculates the next step"
-    if eucl_dist(p1,p2) < 5.0:
+    if eucl_dist(p1,p2) < STEP_LENGTH:
         return p2
     else:
         theta = atan2(p2[1]-p1[1],p2[0]-p1[0])
-        return p1[0] + 5.0*cos(theta), p1[1] + 5.0*sin(theta)
+        return p1[0] + STEP_LENGTH*cos(theta), p1[1] + STEP_LENGTH*sin(theta)
 
 def obstaclefree(p1, p2):
     "checks if there are no walls between two points"
-    t = np.arange(0, 1, 0.05)
+    t = np.arange(0, 1, 0.5)
     for ti in t:
         x = p1[0] + (p2[0] - p1[0]) * ti;
         y = p1[1] + (p2[1] - p1[1]) * ti;
@@ -86,7 +90,7 @@ def obstaclefree(p1, p2):
 
 def main():
     pygame.init()
-    
+
     "Robot node (root of tree)"
     init_node = Node([300, 300], None)
     nodes = []
@@ -102,14 +106,19 @@ def main():
             trashcans[i] = np.random.randint(50, 600-50,2)
 
     "for debug, static trashcan behind wall"
-    trashcans[0] = (500, 180)
+    #trashcans[0] = (500, 180)
+    trashcans[0] = (25, 350)
+    #while True:
+    #    trashcans[0] = (random.randint(0, WIDTH), random.randint(0, HEIGHT))
+    #    if not collides(trashcans[0]):
+    #        break
 
     running = True
     goal = False
 
     goal_node = Node(None, None)
     count = 0
-    
+
     goal_dist = 0
 
     robot = [300, 300]
@@ -119,50 +128,50 @@ def main():
     while running:
 
         if curr_state == 'build':
-            count += 1 
+            count += 1
             if count < MAX_NODES:
                 foundNext = False
                 while foundNext == False:
-                    rand = new_coord() 
+                    rand = new_coord()
                     parent = nodes[0]
 
                     for p in nodes:
                         if eucl_dist(p.coord, rand) <= eucl_dist(parent.coord, rand):
                             newPoint = new_step(p.coord, rand)
-                            if collides(newPoint) == False:
+                            if collides(newPoint) == False and obstaclefree(p.coord, newPoint):
                                 parent = p
                                 foundNext = True
-                            
+
                 newnode = new_step(parent.coord, rand)
                 nodes.append(Node(newnode,parent))
                 pygame.draw.line(screen, (255,255,255), parent.coord, newnode)
-                
+
                 if goal_coll(trashcans[0], newnode, 10):
                     goal = True
                     goal_node = nodes[-1]
                     curr_state = 'goal_found'
-        
+        #xd
         if curr_state == 'goal_found':
 
-            curr_node = goal_node.parent
+            curr_node = goal_node
             while curr_node.parent != None:
                 if curr_node.parent.parent != None and obstaclefree(curr_node.coord, curr_node.parent.parent.coord):
                     curr_node.parent = curr_node.parent.parent
                 else:
                     curr_node = curr_node.parent
 
-            curr_node = goal_node.parent
+            curr_node = goal_node
             while curr_node.parent != None:
                 pygame.draw.line(screen, (150,50,0), curr_node.coord, curr_node.parent.coord, 5)
                 goal_dist += eucl_dist(curr_node.coord, curr_node.parent.coord)
                 curr_node = curr_node.parent
-              
-                #trashcans[0].coord[0] += 
-            
+
+                #trashcans[0].coord[0] +=
+
             running = False
 
         for idx, i in enumerate(trashcans):
-            pygame.draw.circle(screen, (0, 255, 255) , [i[0],i[1]], 10) 
+            pygame.draw.circle(screen, (0, 255, 255) , [i[0],i[1]], 10)
             if (pygame.Rect(i[0],i[1],10,10).colliderect(objRobot)):
                 trashcans = np.delete(trashcans, np.s_[idx:idx+1], axis=0)
                 print("Checked trashcan", idx)
@@ -175,7 +184,6 @@ def main():
 
 
         pygame.display.update()
-
 
     if(goal):
         print("Goal reached in blabla sec, some info")
